7e20d5beb7072ed1c93a106f18ffef4589ed413b
swoody2527 <swoodall2527@gmail.com>
2023-12-20 16:55:05 +0000

    successfully registering account

--- BrickSage/src/components/Register.jsx
+++ BrickSage/src/components/Register.jsx
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import app from '../../config/firebase.js'
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
 
 function Register() {
+  const [email, setEmail] = useState("")
+  const [emailConfirmation, setEmailConfirmation] = useState("")
+  const [password, setPassword] = useState("")
+  const [passwordConfirmation, setPasswordConfirmation] = useState("")
+  const [isEmailMatching, setIsEmailMatching] = useState(false)
+  const [isPasswordMatching, setIsPasswordMatching] = useState(false)
+
+  
   const auth = getAuth(app)
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message
+    })
+
+
+  }
+
   
   return (
     <section>
     <h3>Create an account</h3>
-    <form>
-        <input placeholder='Email'></input>
-        <input placeholder='Confirm Email'></input>
-        <input placeholder='Password'></input>
-        <input placeholder='Confirm Password'></input>
+    <form onSubmit={handleSubmit}>
+        <input placeholder='Email' onChange={(e) => {setEmail(e.target.value)}}></input>
+        <input placeholder='Confirm Email'onChange={(e) => {setEmailConfirmation(e.target.value)}}></input>
+        { isEmailMatching || !email ? null : <p>Email's do not match</p>}
+        <input placeholder='Password'onChange={(e) => {setPassword(e.target.value)}}></input>
+        <input placeholder='Confirm Password'onChange={(e) => {setPasswordConfirmation(e.target.value)}}></input>
+        { isPasswordMatching || !password ? null : <p>Password's do not match</p>}
         <button type='submit'>Register</button>
     </form>
     </section>
